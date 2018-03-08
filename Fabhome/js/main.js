2b9087ee31af2ec8e0d54eb9c384a724a1055bc8
Elina <fedcalmus@gmail.com>
2018-03-08 21:35:38 +0400

    Added footer styles

--- Fabhome/js/main.js
+++ Fabhome/js/main.js
@@ -1,6 +1,6 @@
 
 function initMap() {
-    var ashtarak = {lat: 40.288, lng: 44.293};
+    var ashtarak = {lat: 40.2929, lng: 44.3505};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
         center: ashtarak
